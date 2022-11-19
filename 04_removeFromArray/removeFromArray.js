d2cb2f522c4c5c4410a657d45ed240c795344664
Jonathan <jdneave@gmail.com>
2022-11-18 23:46:20 -0700

    answers

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const arr = args[0];
+    const myArray = [];
 
-};
-
+    arr.forEach((item) => {
+        if (!args.includes(item)) {
+            myArray.push(item);
+        }
+    }); return myArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
