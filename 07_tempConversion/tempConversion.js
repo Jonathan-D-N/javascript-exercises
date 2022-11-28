09a5deda456ae9408ae122d526ca2fc0ef0e80d2
Jonathan <jdneave@gmail.com>
2022-11-28 16:05:31 -0700

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const ftoc = function() {
+const ftoc = function(F) {
+  let faren = ((F-32) * 5 / 9)
+    if (faren % 1 == 0) {
+      return(faren)
+    } else {
+      faren = +faren.toFixed(1)
+      return(faren)
+    }
 
 };
 
-const ctof = function() {
-
+const ctof = function(C) {
+  let celcius = (C *(9/5) + 32)
+  if (celcius % 1 == 0) {
+    return(celcius)
+  } else {
+    celcius = +celcius.toFixed(1)
+  return(celcius);
+  }
 };
 
 // Do not edit below this line
