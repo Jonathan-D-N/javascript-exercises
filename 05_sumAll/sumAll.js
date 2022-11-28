2f00851aad7b29a5499389eca95a4c3a33547d18
Jonathan <jdneave@gmail.com>
2022-11-28 01:05:36 -0700

    solved 05 and 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(num1, num2) {
+    let lowEnd = num1;
+    let highEnd = num2;
+    let list = (0);
+    if (typeof lowEnd === 'number' && typeof highEnd === 'number' && lowEnd >= 0 && highEnd >= 0) {
+        const loopSum = function() {
+            if (lowEnd < highEnd) {
+            for (i = lowEnd; i <= highEnd; i++) {
+                list += i;
+            } 
+        } else {
+            for (i = lowEnd; i >= highEnd; i--) {
+                list += i;
+        }   
+        }
+        }
+        loopSum()
+        return(list);
+    } else {
+        return('ERROR');
+    }
+}
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(arg) {
+const year = arg
+//if year is divisible by 4, 4 and 400, and not divisible by 100, then return true
+if (year % 400 == 0 && year && year % 4 == 0) {
+return true
+} else if (year % 4 == 0 && year % 100 !== 0) {
+return true
+} else {
+    return false
+}
+}
 // Do not edit below this line
 module.exports = leapYears;
