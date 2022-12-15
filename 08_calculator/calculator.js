02e74e711b1dbb7c27c78692b63873b4082bb829
Jonathan <jdneave@gmail.com>
2022-12-14 23:08:05 -0700

    solved calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  const initialValue = 0;
+  const sumWithInitial = a.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, initialValue
+  );
+  return sumWithInitial;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  if (a.length > 0) {
+    return a.reduce((accumulator, nextItem) => accumulator * nextItem)
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b
 };
 
-const factorial = function() {
-	
+
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  } else if (a === 1) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
